1fcb856216b3cdc50b4f97f2f4a440065b445168
lynxinfiniteconta-ui <lynx.infinite.conta@gmail.com>
2025-08-13 17:18:25 -0300

    Update SocialFooter.tsx

--- src/components/SocialFooter.tsx
+++ src/components/SocialFooter.tsx
@@ -45,7 +45,6 @@ const SocialFooter = () => {
         animate={{ opacity: 1 }}
         transition={{ duration: 0.6, delay: 1.8 }}
       >
-        © 2024 - Criado com ❤️
       </motion.p>
     </motion.footer>
   );
