eb2dc882355e88269a3ee496fadfaa6fcc5f633d
lynxinfiniteconta-ui <lynx.infinite.conta@gmail.com>
2025-08-13 17:14:53 -0300

    Update ProfileSection.tsx

--- src/components/ProfileSection.tsx
+++ src/components/ProfileSection.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import profilePhoto from 'Prancheta200.jpg';
+import profilePhoto from '@/assets/profile-photo.jpg';
 
 const ProfileSection = () => {
   return (
